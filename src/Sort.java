85b8884ac29b46b38fe3f3a7fb1b454d794fe8d7
Joshua Shew <joshua.t.shew@gmail.com>
2022-04-14 18:30:51 -0400

    a

--- src/Sort.java
+++ src/Sort.java
@@ -1,4 +1,11 @@
 import java.util.Random;
+import java.lang.reflect.Method;
+import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import com.opencsv.CSVWriter;
+import java.io.FileWriter;
+import java.io.File;
 
 public class Sort {
 
@@ -25,12 +32,13 @@ public class Sort {
      * @return an int[] of random integers
      */
     public static int[] randomArray(int n, int a, int b) {
-        if (b < a) {
+        if (b < a) { // check for invalid input
             return new int[0];
         }
 
         int[] result = new int[n];
         
+        /* generate values for each value of the array*/
         for (int i = 0; i < n; i++) {
             result[i] = a + (int)((new Random()).nextDouble() * (b - a + 1));
         }
@@ -40,6 +48,8 @@ public class Sort {
 
     /**
      * Sorts the input array using insertionSort
+     * This checks each value (left to right) and finds a place in the sorted portion of the array (on the left) to insert the element into
+     * Shifts elements down to make space for the insertion
      * @param arr the array to sort
      */
     public static void insertionSort(int[] arr) {
@@ -47,8 +57,11 @@ public class Sort {
             return;
         }
         
+        /* i goes through each value that is to be inserted
+         * everything to the left of i is sorted */
         for (int i = 1; i < arr.length; i++) {
             int j = i - 1;
+            /* j goes back to find a space for the new element */
             while (j >= 0 && arr[j] > arr[j + 1]) {
                 swap(arr, j, j + 1);
                 j--;
@@ -60,6 +73,7 @@ public class Sort {
     
     /**
      * Sorts the input array using quickSort
+     * quickSort divides the array into halves (left <= right) until the sub-array size is 1, and as a result of the dividing conditions, the array is sorted
      * @param arr the array to sort
      */
     public static void quickSort(int[] arr) {
@@ -70,11 +84,18 @@ public class Sort {
         qSort(arr, 0, arr.length - 1);
     }
 
+    /**
+     * Helper method for quickSort that allows the array to be divided (and then conquered)
+     * @param arr the array to sort
+     * @param start beginning (inclusive) of section to sort
+     * @param end ending (inclusive) of section to sort
+     */
     private static void qSort(int[] arr, int start, int end) {
-        if (end - start < 1) {
+        if (end - start < 1) { // base case
             return;
         }
 
+        // selects a random pivot
         int pivot = arr[start + (int)((new Random()).nextDouble() * (end - start + 1))];
 
         int partition = partition(arr, pivot, start, end);
@@ -83,9 +104,18 @@ public class Sort {
         qSort(arr, partition + 1, end);
     }
 
+    /**
+     * Partitions an array (or part of 1) into two halves and finds the division
+     * Left is less than or equal to the pivot, right is greater than or equal to the pivot
+     * @param arr the array the values are from
+     * @param pivot the pivot
+     * @param start the left hand boundary (inclusive)
+     * @param end the right hand boundary (inclusive)
+     * @return the upper boundary (inclusive) of the left sub-array after partitioning
+     */
     private static int partition(int[] arr, int pivot, int start, int end) {
-        int i = start - 1;
-        int j = end + 1;
+        int i = start - 1; // i finds values that need to be moved to the right sub-array
+        int j = end + 1; // j finds values that need to be moved to the left sub-array
 
         while (true) {
             do {
@@ -95,10 +125,10 @@ public class Sort {
                 j--;
             } while (j > -1 && arr[j] > pivot);
 
-            if (i > j) {
+            if (i > j) { // if they have crossed over, then the partition has been found
                 return j;
             }
-            else {
+            else { // swap if both i and j have values they can swap over
                 swap(arr, i, j);
             }
         }
@@ -106,6 +136,8 @@ public class Sort {
 
     /**
      * Sorts the input array using mergeSort
+     * Divides the array into sub-arrays until size is 1 (an array of 1 is sorted)
+     * Recombines them into larger sorted sub-arrays until the whole array is sorted
      * @param arr the array to sort
      */
     public static void mergeSort(int[] arr) {
@@ -113,13 +145,22 @@ public class Sort {
             return;
         }
 
+        /* this temporary array is necessary for efficiently combining two sorted subarrays
+         * enough space is created to allow for all the recursive calls to reference their part without overlapping */
         int[] temp = new int[arr.length];
 
         mSort(arr, temp, 0, arr.length - 1);
     }
 
+    /**
+     * Helper method for mergeSort
+     * @param arr the array to sort
+     * @param temp 
+     * @param start
+     * @param end
+     */
     private static void mSort(int[] arr, int[] temp, int start, int end) {
-        if (end <= start) {
+        if (end <= start) { // base case
             return;
         }
         
@@ -132,12 +173,21 @@ public class Sort {
         merge(arr, temp, start, split, split + 1, end);
     }
 
+    /**
+     * Merges two (sorted) sub-arrays into one sorted array by using a temporary array as space to process the values
+     * @param arr the array to sort
+     * @param temp the temporary array
+     * @param leftStart the lower boundary (inclusive) of the left sub-array
+     * @param leftEnd the upper boundary (inclusive) of the right sub-array
+     * @param rightStart the lower boundary (inclusive) of the right sub-array
+     * @param rightEnd the upper boundary (inclusive) of the right sub-array
+     */
     private static void merge(int[] arr, int[] temp, int leftStart, int leftEnd, int rightStart, int rightEnd) {
         // merge
         int i = leftStart;
         int j = rightStart;
         int k = leftStart;
-        while (i <= leftEnd && j <= rightEnd) {
+        while (i <= leftEnd && j <= rightEnd) { // compare values to copy over the lower one
             if (arr[i] < arr[j]) {
                 temp[k++] = arr[i++];
             }
@@ -145,10 +195,10 @@ public class Sort {
                 temp[k++] = arr[j++];
             }
         }
-        while (i <= leftEnd) {
+        while (i <= leftEnd) { // copy over remaining
             temp[k++] = arr[i++];
         }
-        while (j <= rightEnd) {
+        while (j <= rightEnd) { // copy over remaining
             temp[k++] = arr[j++];
         }
 
@@ -160,6 +210,8 @@ public class Sort {
 
     /**
      * Sorts the input array using bubbleSort
+     * Repeatedly goes through the array from left to right, swapping adjacent values if they are out of order
+     * As a result, by the k-th iteration, the k right-most elements are sorted
      * @param arr the array to sort
      */
     public static void bubbleSort(int[] arr) {
@@ -167,7 +219,9 @@ public class Sort {
             return;
         }
 
+        /* repeated arr.length times to make sure the smallest value on the rightmost side can make it all the way over */
         for (int i = 0; i < arr.length; i++) {
+            /* goes through left to right, swapping values */
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     swap(arr, j, j + 1);
@@ -177,8 +231,101 @@ public class Sort {
     }
 
     /**
+     * Java's built in sort method
+     * @param arr the array to sort
+     */
+    public static void javaSort(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    private static void benchmark() throws Exception {
+        String[] methodNames = new String[]{"insertionSort", "quickSort", "mergeSort", "bubbleSort", "javaSort"};
+        int[] sizes = new int[]{10, 20, 50, 100, 200, 500, 1000, 2000, 5000};
+        int repetitions = 100;
+
+        Hashtable<String, ArrayList<Long>> results = new Hashtable<String, ArrayList<Long>>();
+
+        // time all methods
+        for (String methodName : methodNames) {
+            results.put(methodName, new ArrayList<Long>());
+
+            Method method = Sort.class.getDeclaredMethod(methodName, int[].class);
+            method.setAccessible(true);
+
+            for (int size : sizes) {
+                long totalTime = 0L;
+                
+                for (int i = 0; i < repetitions; i++) {
+                    int[] a1 = Sort.randomArray(size, 0, 100);
+                    long start;
+                    long time;
+
+                    start = System.nanoTime();
+                    method.invoke(null, a1);
+                    time = System.nanoTime() - start;
+
+                    totalTime += time;
+                }
+
+                long averageTime = totalTime / repetitions;
+                results.get(methodName).add(averageTime);
+            }
+        }
+
+        // print out
+        for (String methodName : methodNames) {
+            System.out.println(methodName);
+
+            for (int i = 0; i < sizes.length; i++) {
+                System.out.print(sizes[i] + ": ");
+                System.out.print(results.get(methodName).get(i) + "\n");
+            }
+
+            System.out.println("");
+        }
+
+        // print out as csv
+        
+        File resultsFile = new File("benchmarking.csv");
+        resultsFile.createNewFile();
+        CSVWriter csvWriter = new CSVWriter(new FileWriter(resultsFile));
+
+        // CSVWriter csvWriter = new CSVWriter(new FileWriter(Files.createFile(Paths.get("/benchmarking.csv")).toString()));
+        
+        String[] nextLine;
+
+        nextLine = new String[sizes.length + 1];
+        for (int i = 0; i < sizes.length; i++) {
+            nextLine[i + 1] = Integer.toString(sizes[i]);
+        }
+        csvWriter.writeNext(nextLine);
+
+        for (String methodName : methodNames) {
+            nextLine = new String[sizes.length + 1];
+
+            nextLine[0] = methodName;
+
+            ArrayList<Long> resultsList = results.get(methodName);
+
+            for (int i = 0; i < resultsList.size(); i++) {
+                nextLine[i + 1] = Long.toString(resultsList.get(i));
+            }
+
+            csvWriter.writeNext(nextLine);
+        }
+        
+        csvWriter.close();
+    }
+
+    /**
      * Main method to demonstrate the functionality of all the methods
      */
     public static void main(String[] args) throws Exception {
+        if (args[0].equals("benchmark")) {
+            benchmark();
+        }
+        else {
+
+        }
     }
 }
