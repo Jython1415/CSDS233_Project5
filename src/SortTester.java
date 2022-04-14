2e81a7834940137063b1bdfc03e245cca014e6fd
Joshua Shew <joshua.t.shew@gmail.com>
2022-04-14 11:03:25 -0400

    SortTester outline

---
+++ src/SortTester.java
@@ -0,0 +1,45 @@
+import org.junit.Test;
+import org.junit.Assert;
+
+public class SortTester {
+    
+    /**
+     * Unit tests for randomArray
+     */
+    @Test
+    public void testRandomArray() {
+        
+    }
+
+    /**
+     * Unit tests for insertionSort
+     */
+    @Test
+    public void testInsertionSort() {
+
+    }
+
+    /**
+     * Unit tests for quickSort
+     */
+    @Test
+    public void testQuickSort() {
+
+    }
+
+    /**
+     * Unit tests for mergeSort
+     */
+    @Test
+    public void testMergeSort() {
+
+    }
+
+    /**
+     * Unit tests for bubbleSort
+     */
+    @Test
+    public void testBubbleSort() {
+
+    }
+}
