daf290abb47dd3e6743d48a910873140cd4cca77
Joshua Shew <joshua.t.shew@gmail.com>
2022-04-14 11:52:17 -0400

    insertionSort tests

--- src/SortTester.java
+++ src/SortTester.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 import org.junit.Assert;
+import java.util.Random;
 
 public class SortTester {
     
@@ -9,7 +10,7 @@ public class SortTester {
      * @param length the length of the array
      * @return an array of ascending integers
      */
-    private int[] ascendingArray(int start, int length) {
+    private static int[] ascendingArray(int start, int length) {
         int[] result = new int[length];
         
         for (int i = 0; i < length; i++) {
@@ -25,7 +26,7 @@ public class SortTester {
      * @param length the length of the array
      * @return an array of descending integers
      */
-    private int[] descendingArray(int start, int length) {
+    private static int[] descendingArray(int start, int length) {
         int[] result = new int[length];
         
         for (int i = 0; i < length; i++) {
@@ -36,11 +37,51 @@ public class SortTester {
     }
 
     /**
+     * Creating an array of repeated integers of the specified length
+     * @param value the value to repeat
+     * @param length the length of the array
+     * @return an array of the input value 
+     */
+    private static int[] repeatingArray(int value, int length) {
+        int[] result = new int[length];
+        
+        for (int i = 0; i < length; i++) {
+            result[i] = value;
+        }
+
+        return result;
+    }
+
+    /**
      * Swaps 2 random values in the input array
      * @param arr the array to swap values in
      */
-    private void randomSwap(int[] arr) {
+    private static int[] randomSwap(int[] arr) {
+        int a = (int)((new Random()).nextDouble() * arr.length);
+        int b;
 
+        do {
+            b = (int)((new Random()).nextDouble() * arr.length);
+        } while (b == a);
+
+        int save = arr[a];
+        arr[a] = arr[b];
+        arr[b] = save;
+
+        return arr;
+    }
+
+    /**
+     * Performs n random swaps on the input array to randomize it
+     * @param arr the array to randomize
+     * @param n the number of random swaps
+     */
+    private static int[] randomizeArray(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            randomSwap(arr);
+        }
+
+        return arr;
     }
 
     /**
@@ -86,7 +127,61 @@ public class SortTester {
      */
     @Test
     public void testInsertionSort() {
+        int[] a1;
+        
+        // null
+        a1 = null;
+        Sort.insertionSort(a1);
+        Assert.assertArrayEquals(null, a1);
+        
+        // length 0
+        a1 = new int[0];
+        Sort.insertionSort(a1);
+        Assert.assertArrayEquals(new int[0], a1);
 
+        // length 1
+        a1 = new int[]{0};
+        Sort.insertionSort(a1);
+        Assert.assertArrayEquals(new int[]{0}, a1);
+
+        // length 2
+        a1 = new int[]{0, 1};
+        Sort.insertionSort(a1);
+        Assert.assertArrayEquals(new int[]{0, 1}, a1);
+        a1 = new int[]{1, 0};
+        Sort.insertionSort(a1);
+        Assert.assertArrayEquals(new int[]{0, 1}, a1);
+        a1 = new int[]{0, 0};
+        Sort.insertionSort(a1);
+        Assert.assertArrayEquals(new int[]{0, 0}, a1);
+
+        // large even length - ascending, descending, repeated, and randomized
+        a1 = ascendingArray(0, 1000);
+        Sort.insertionSort(a1);
+        Assert.assertArrayEquals(ascendingArray(0, 1000), a1);
+        a1 = descendingArray(0, 1000);
+        Sort.insertionSort(a1);
+        Assert.assertArrayEquals(ascendingArray(0, 1000), a1);
+        a1 = repeatingArray(0, 1000);
+        Sort.insertionSort(a1);
+        Assert.assertArrayEquals(repeatingArray(0, 1000), a1);
+        a1 = randomizeArray(ascendingArray(0, 1000), 1000);
+        Sort.insertionSort(a1);
+        Assert.assertArrayEquals(ascendingArray(0, 1000), a1);
+
+        // large odd length - ascending, descending, repeated, and randomized
+        a1 = ascendingArray(0, 1001);
+        Sort.insertionSort(a1);
+        Assert.assertArrayEquals(ascendingArray(0, 1001), a1);
+        a1 = descendingArray(0, 1001);
+        Sort.insertionSort(a1);
+        Assert.assertArrayEquals(ascendingArray(0, 1001), a1);
+        a1 = repeatingArray(0, 1001);
+        Sort.insertionSort(a1);
+        Assert.assertArrayEquals(repeatingArray(0, 1001), a1);
+        a1 = randomizeArray(ascendingArray(0, 1001), 1001);
+        Sort.insertionSort(a1);
+        Assert.assertArrayEquals(ascendingArray(0, 1001), a1);
     }
 
     /**
